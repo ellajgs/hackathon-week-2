5ecb51e4eabf425854df7438417fcdf8fdc49bae
ella <ejgsimmons@outlook.com>
2026-05-01 11:39:42 +0100

    get game to work

--- index.js
+++ index.js
@@ -1,59 +1,51 @@
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const playerOptions = [rockBtn, paperBtn, scissorsBtn];
+const computerOptions = ["rock", "paper", "scissors"];
+const choices = document.querySelector("#choices");
 
-  const rockBtn = document.querySelector("#rock");
-  const paperBtn = document.querySelector("#paper");
-  const scissorsBtn = document.querySelector("#scissors");
-  const playerOptions = [rockBtn, paperBtn, scissorsBtn];
-  const computerOptions = ["rock", "paper", "scissors"];
-  const choices = document.querySelector("#choices")
-
-
-  playerOptions.forEach(option => {
-    option.addEventListener("click", playGame)
-  })
+playerOptions.forEach((option) => {
+  option.addEventListener("click", playGame);
+});
 
 function playGame(e) {
-    const randomNum = Math.floor(Math.random() * computerOptions.length )
-    const computerChoice = computerOptions[randomNum] 
-    const writeComputerChoice = document.querySelector("#computer-choice")
-    
-    const playerChoice =e.target.id
-    const writePlayerChoice = document.querySelector("#play-choice")
-    const writeResult = document.querySelector('#resultDisplay')
+  const randomNum = Math.floor(Math.random() * computerOptions.length);
+  const computerChoice = computerOptions[randomNum];
+  const writeComputerChoice = document.querySelector("#computer-choice");
 
-    
-    
-    // resultDisplay.textContent = result
-    writePlayerChoice.textContent = `PLAYER: ${playerChoice}`
-    writeComputerChoice.textContent = `COMPUTER: ${computerChoice}`
-    writeResult.textContent = `RESULT: ${result}`
-    
-    
+  const playerChoice = e.target.id;
+  const writePlayerChoice = document.querySelector("#play-choice");
+  const writeResult = document.querySelector("#resultDisplay");
 
-    const result = (playerChoice, computerChoice) => {
-    
-  if (playerChoice === "rock" && computerChoice === "scissors") {
-    return "Player 1 won!";
-  } else if (playerChoice === "rock" && computerChoice === "paper") {
-    return "Player 2 won!";
-  } else if (playerChoice === "rock" && computerChoice === "rock") {
-    return "Draw!";
-  } else if (playerChoice === "scissors" && computerChoice === "paper") {
-    return "Player 1 won!";
-  } else if (playerChoice === "scissors" && computerChoice === "scissors") {
-    return "Draw!";
-  } else if (playerChoice === "scissors" && computerChoice === "rock") {
-    return "Player 2 won!";
-  } else if (playerChoice === "paper" && computerChoice === "paper") {
-    return "Draw!";
-  } else if (playerChoice === "paper" && computerChoice === "scissors") {
-    return "Player 2 won!";
-  } else if (playerChoice === "paper" && computerChoice === "rock") {
-    return "Player 1 won!";
-  }
-};
+  // resultDisplay.textContent = result
+  writePlayerChoice.textContent = `PLAYER: ${playerChoice}`;
+  writeComputerChoice.textContent = `COMPUTER: ${computerChoice}`;
 
+  const result = score(playerChoice, computerChoice);
+
+  writeResult.textContent = `RESULT: ${result}`;
 }
-
-
-
-  
+function score(playerChoice, computerChoice) {
+  let result = "";
+  if (playerChoice === "rock" && computerChoice === "scissors") {
+    result = "You won!";
+  } else if (playerChoice === "rock" && computerChoice === "paper") {
+    result = "You lost!";
+  } else if (playerChoice === "rock" && computerChoice === "rock") {
+    result = "Draw!";
+  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+    result = "You won!";
+  } else if (playerChoice === "scissors" && computerChoice === "scissors") {
+    result = "Draw!";
+  } else if (playerChoice === "scissors" && computerChoice === "rock") {
+    result = "You lost!";
+  } else if (playerChoice === "paper" && computerChoice === "paper") {
+    result = "Draw!";
+  } else if (playerChoice === "paper" && computerChoice === "scissors") {
+    result = "You lost!";
+  } else if (playerChoice === "paper" && computerChoice === "rock") {
+    result = "You won!";
+  }
+  return result;
+}
