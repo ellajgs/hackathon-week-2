cfe03bb2eb8a9c9cc1039ed797ea79b529de4d9b
ella <ejgsimmons@outlook.com>
2026-05-01 12:12:06 +0100

    update scores

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+let playerScore = 0
+let computerScore = 0
+
 const rockBtn = document.querySelector("#rock");
 const paperBtn = document.querySelector("#paper");
 const scissorsBtn = document.querySelector("#scissors");
 const playerOptions = [rockBtn, paperBtn, scissorsBtn];
 const computerOptions = ["rock", "paper", "scissors"];
 const choices = document.querySelector("#choices");
+
+const playerScoreDisplay = document.querySelector("#player-score");
+const computerScoreDisplay = document.querySelector("#computer-score");
 
 playerOptions.forEach((option) => {
   option.addEventListener("click", playGame);
@@ -23,29 +29,36 @@ function playGame(e) {
   writeComputerChoice.textContent = `COMPUTER: ${computerChoice}`;
 
   const result = score(playerChoice, computerChoice);
-
   writeResult.textContent = `RESULT: ${result}`;
-}
+  playerScoreDisplay.textContent = playerScore
+  computerScoreDisplay.textContent = computerScore
 function score(playerChoice, computerChoice) {
   let result = "";
   if (playerChoice === "rock" && computerChoice === "scissors") {
     result = "You won!";
+    playerScore++
   } else if (playerChoice === "rock" && computerChoice === "paper") {
     result = "You lost!";
+    computerScore++
   } else if (playerChoice === "rock" && computerChoice === "rock") {
     result = "Draw!";
   } else if (playerChoice === "scissors" && computerChoice === "paper") {
     result = "You won!";
+    playerScore++
   } else if (playerChoice === "scissors" && computerChoice === "scissors") {
     result = "Draw!";
   } else if (playerChoice === "scissors" && computerChoice === "rock") {
     result = "You lost!";
+    computerScore++
   } else if (playerChoice === "paper" && computerChoice === "paper") {
     result = "Draw!";
   } else if (playerChoice === "paper" && computerChoice === "scissors") {
     result = "You lost!";
+    computerScore++
   } else if (playerChoice === "paper" && computerChoice === "rock") {
     result = "You won!";
+    playerScore++
   }
   return result;
+}
 }
