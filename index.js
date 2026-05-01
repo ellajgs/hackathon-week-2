a6e5e995f89a6e3356a305ba979dd279431a283c
ella <ejgsimmons@outlook.com>
2026-05-01 10:28:43 +0100

    get computer to pick random

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-const rps = (p1, p2) => {
-  if (p1 === "rock" && p2 === "scissors") {
-    return "Player 1 won!";
-  } else if (p1 === "rock" && p2 === "paper") {
-    return "Player 2 won!";
-  } else if (p1 === "rock" && p2 === "rock") {
-    return "Draw!";
-  } else if (p1 === "scissors" && p2 === "paper") {
-    return "Player 1 won!";
-  } else if (p1 === "scissors" && p2 === "scissors") {
-    return "Draw!";
-  } else if (p1 === "scissors" && p2 === "rock") {
-    return "Player 2 won!";
-  } else if (p1 === "paper" && p2 === "paper") {
-    return "Draw!";
-  } else if (p1 === "paper" && p2 === "scissors") {
-    return "Player 2 won!";
-  } else if (p1 === "paper" && p2 === "rock") {
-    return "Player 1 won!";
-  }
-};
+
+  const rockBtn = document.querySelector("#rock");
+  const paperBtn = document.querySelector("#paper");
+  const scissorsBtn = document.querySelector("#scissors");
+  const playerOptions = [rockBtn, paperBtn, scissorsBtn];
+  const computerOptions = ["rock", "paper", "scissors"];
+
+  playerOptions.forEach(option => {
+    option.addEventListener("click", playGame)
+  })
+
+function playGame() {
+    const randomNum = Math.floor(Math.random() * computerOptions.length )
+    const computerChoice = computerOptions[randomNum]
+    
+    
+
+}
+
+// const rps = (p1, p2) => {
+//   if (p1 === "rock" && p2 === "scissors") {
+//     return "Player 1 won!";
+//   } else if (p1 === "rock" && p2 === "paper") {
+//     return "Player 2 won!";
+//   } else if (p1 === "rock" && p2 === "rock") {
+//     return "Draw!";
+//   } else if (p1 === "scissors" && p2 === "paper") {
+//     return "Player 1 won!";
+//   } else if (p1 === "scissors" && p2 === "scissors") {
+//     return "Draw!";
+//   } else if (p1 === "scissors" && p2 === "rock") {
+//     return "Player 2 won!";
+//   } else if (p1 === "paper" && p2 === "paper") {
+//     return "Draw!";
+//   } else if (p1 === "paper" && p2 === "scissors") {
+//     return "Player 2 won!";
+//   } else if (p1 === "paper" && p2 === "rock") {
+//     return "Player 1 won!";
+//   }
+// };
